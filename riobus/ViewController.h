ed16312aaac938685e0cf39853c121343b3c5475
brunodoamaral <brunodoamaral@mac-bruno>
2014-07-04 14:56:38 -0300

    Initial Commit

---
+++ riobus/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  riobus
+//
+//  Created by Bruno do Amaral on 04/07/2014.
+//  Copyright (c) 2014 Rio Bus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ riobus/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  riobus
+//
+//  Created by Bruno do Amaral on 04/07/2014.
+//  Copyright (c) 2014 Rio Bus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
